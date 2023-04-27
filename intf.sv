cbf7f219e47f82416db8ecbf5047d7599e5409e1
vivek kumar <vivekkr626@gmail.com>
2023-04-27 15:42:48 +0530

    Create intf.sv

---
+++ intf.sv
@@ -0,0 +1,12 @@
+//create a interface
+//remember to decalare clk, coming from top module
+//every signal that comes from top module need to declare as input
+Interface our_interface(input );
+ //input_1/2
+logic input_1
+logic input_2
+
+//output
+Output output_3
+
+endinterface
