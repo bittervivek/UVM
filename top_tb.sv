829ca2a396bb129d57e3941d01a74f3934fc4a9a
vivek kumar <vivekkr626@gmail.com>
2023-04-27 15:37:02 +0530

    Update top_tb.sv

--- top_tb.sv
+++ top_tb.sv
@@ -1,33 +1,31 @@
-`include "interfc.sv"
-`include "agnt.sv"
-`include "drv.sv"
-`include "env.sv"
-`include "mon.sv"
-`include "scbd.sv"
-`include "seq.sv"
-`include "seq_item.sv"
-`include "test.sv"
-`include "trans.sv"
+Top.sv – static component // contains design and test component as per last slide
+// in the testbench top module, the DUT will be connect to the interface object
 
-module top_tb();
-	dff_if vif();
+	`include “uvm_micros.svh”
+	import uvm_pkg::*;
+	module top();
 	
-	dff_rtl DUT (
-				.clk(vif.clk),
-				.rst(vif.rst),
-				.din(vif.din),
-				.dout(vif.dout)
-				);
+	our_interface intf(); // intantiate our interface
+	//instantiate design
+	jkff dut(intf.input_1,
+		intf.input_2,
+		intf.output_3); 
+	//clk generation and supplied to DUT
+	initial begin
+		intf.clk = 0;
+		forever #5 intf.clk = ~intf.clk;
+	end
 	
-	initial
-	begin: B1
-	
-	 vif.clk = '0;
-	 uvm_config_db #(virtual dff_if)::set(null,"","virtual_intf",vif);
-	 
-	 run_test("test_one");
-	end:  B1
-	
-	always #10 vif.clk = ~vif.clk;
-	
-endmodule: top_tb
+	//use set method in top module to set the interface in db
+	initial begin
+	 uvm_config_db #(virtual our_interface)::set(null, “*”, “intf”, intf);
+	end
+	//test component – dynamic component
+	initial begin
+// run_test method is actually a task defined in the uvm_globals.svh which is imported by this statement import uvm_pkg::*.
+	run_test(“our_test”);
+	end
+
+	//build phase
+	//connect phase
+	endmodule
